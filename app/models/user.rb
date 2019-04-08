303cdf3f5dabd4ce331731d8e2a6681a839739e4
Jean Ha <jeanha124@gmail.com>
2019-04-08 13:41:51 -0400

    "Rebundled"

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   has_many :albums
   has_many :comments
   has_many :tags
-  has_one_attached :avatar
+  # has_one_attached :avatar
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
